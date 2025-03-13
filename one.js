eb5d2a63aa53d3e2c89611d6d9d18ef6d593599d
SamataHS <samatahs50@gmail.com>
2025-03-13 11:55:40 -0700

    hi

---
+++ one.js
@@ -0,0 +1,19 @@
+// let a=2;
+// let b=3;
+// console.log(a+b); 
+// let expo=(x,y)=>{
+//     return x**y;
+// }
+// console.log(expo(2,3));
+// let a=(5>2);
+// console.log(a);
+// console.log(typeof(a));
+let obj={
+    name:"sai",
+    age:23, 
+    greet:function(){
+        console.log("hello");
+    }
+}
+console.log(obj);
+obj.greet();
